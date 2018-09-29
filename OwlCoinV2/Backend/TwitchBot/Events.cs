ca0487c59cecc60a5947a419d291f9baec4eb90c
Unknown <oscar.davies@hotmail.co.uk>
2018-09-29 09:33:45 +0100

    Now Actually Finished (Twitch)

--- OwlCoinV2/Backend/TwitchBot/Events.cs
+++ OwlCoinV2/Backend/TwitchBot/Events.cs
@@ -15,31 +15,34 @@ namespace OwlCoinV2.Backend.TwitchBot
         {
             string Id = UserHandler.UserFromUsername(e.GiftedSubscription.DisplayName).Matches[0].Id;
             Shared.Data.UserData.CreateUser(Id, Shared.IDType.Twitch);
-            Shared.Data.Accounts.GiveUser(Id, Shared.IDType.Twitch, 1000);
-            Bot.TwitchC.SendMessage(e.Channel, "@" + e.GiftedSubscription.DisplayName + " Gifted a sub too @" + e.GiftedSubscription.MsgParamRecipientDisplayName + " and received 1000 Owlcoin!");
+            int Reward = int.Parse(Shared.ConfigHandler.Config["Rewards"]["Twitch"]["GiftedSub"].ToString());
+            Shared.Data.Accounts.GiveUser(Id, Shared.IDType.Twitch, Reward);
+            MessageHandler.SendMessage(e.Channel, Shared.ConfigHandler.Config["EventMessages"]["GiftedSub"].ToString(), e.GiftedSubscription.MsgParamRecipientDisplayName, Reward);
         }
 
         public static void Subbed(object sender, OnNewSubscriberArgs e)
         {
             Shared.Data.UserData.CreateUser(e.Subscriber.UserId, Shared.IDType.Twitch);
-            Shared.Data.Accounts.GiveUser(e.Subscriber.UserId, Shared.IDType.Twitch, 1000);
-            Bot.TwitchC.SendMessage(e.Channel, "@" + e.Subscriber.DisplayName + " Subbed for the first time and received 1000 Owlcoin!");
+            int Reward = int.Parse(Shared.ConfigHandler.Config["Rewards"]["Twitch"]["Subbed"].ToString());
+            Shared.Data.Accounts.GiveUser(e.Subscriber.UserId, Shared.IDType.Twitch, Reward);
+            MessageHandler.SendMessage(e.Channel, Shared.ConfigHandler.Config["EventMessages"]["GiftedSub"].ToString(),null, Reward);
         }
         public static void ReSubbed(object sender,OnReSubscriberArgs e)
         {
             Shared.Data.UserData.CreateUser(e.ReSubscriber.UserId, Shared.IDType.Twitch);
-            Shared.Data.Accounts.GiveUser(e.ReSubscriber.UserId, Shared.IDType.Twitch, 1000);
-            Bot.TwitchC.SendMessage(e.Channel, "@" + e.ReSubscriber.DisplayName + " Resubbed and received 1000 Owlcoin!");
+            int Reward = int.Parse(Shared.ConfigHandler.Config["Rewards"]["Twitch"]["ReSubbed"].ToString());
+            Shared.Data.Accounts.GiveUser(e.ReSubscriber.UserId, Shared.IDType.Twitch, Reward);
+            MessageHandler.SendMessage(e.Channel, Shared.ConfigHandler.Config["EventMessages"]["GiftedSub"].ToString(), null, Reward);
         }
-        static List<String[]> Hosts = new List<string[]> { };
-        public static void Hosting(object sender,OnBeingHostedArgs e)
-        {
-            if (Hosts.Contains(new string[] { e.BeingHostedNotification.HostedByChannel,DateTime.Now.DayOfYear.ToString() })) { return; }
-            Shared.Data.UserData.CreateUser(e.BeingHostedNotification.HostedByChannel, Shared.IDType.Twitch);
-            Shared.Data.Accounts.GiveUser(e.BeingHostedNotification.HostedByChannel, Shared.IDType.Twitch, 250);
-            Bot.TwitchC.SendMessage(e.BeingHostedNotification.Channel, "@" + e.BeingHostedNotification.HostedByChannel + " Started hosting with "+e.BeingHostedNotification.Viewers+" viewers and received 250 Owlcoin!");
-            Hosts.Add(new string[] { e.BeingHostedNotification.HostedByChannel,DateTime.Now.DayOfYear.ToString() });
-        }
+        //static List<String[]> Hosts = new List<string[]> { };
+        //public static void Hosting(object sender,OnBeingHostedArgs e)
+        //{
+        //    if (Hosts.Contains(new string[] { e.BeingHostedNotification.HostedByChannel,DateTime.Now.DayOfYear.ToString() })) { return; }
+        //    Shared.Data.UserData.CreateUser(e.BeingHostedNotification.HostedByChannel, Shared.IDType.Twitch);
+        //    Shared.Data.Accounts.GiveUser(e.BeingHostedNotification.HostedByChannel, Shared.IDType.Twitch, 250);
+        //    Bot.TwitchC.SendMessage(e.BeingHostedNotification.Channel, "@" + e.BeingHostedNotification.HostedByChannel + " Started hosting with "+e.BeingHostedNotification.Viewers+" viewers and received 250 Owlcoin!");
+        //    Hosts.Add(new string[] { e.BeingHostedNotification.HostedByChannel,DateTime.Now.DayOfYear.ToString() });
+        //}
 
     }
 }
