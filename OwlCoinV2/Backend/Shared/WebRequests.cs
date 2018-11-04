a870c7bd359f766f66e656c0bed9fcf3a0282974
Jaminima <oscar.william.davies@outlook.com>
2018-11-04 12:31:43 +0000

    Added response Caching, to reduce the number of requests sent to the API

--- OwlCoinV2/Backend/Shared/WebRequests.cs
+++ OwlCoinV2/Backend/Shared/WebRequests.cs
@@ -12,16 +12,19 @@ namespace OwlCoinV2.Backend.Shared
     {
         public static Newtonsoft.Json.Linq.JToken GET(string URL)
         {
+            if (CachedObjectExists(URL, null, null)) { return GetCachedResponse(URL, null, null); }
             WebRequest Request = WebRequest.Create(ConfigHandler.Config["API"]["URL"].ToString() + ":" + ConfigHandler.Config["API"]["Port"] + URL);
             Request.Method = "GET";
             WebResponse Response = Request.GetResponse();
             string D = new StreamReader(Response.GetResponseStream()).ReadToEnd();
             Newtonsoft.Json.Linq.JToken JD = Newtonsoft.Json.Linq.JToken.Parse(D);
+            CacheResponse(URL, null, null, JD);
             return JD;
         }
 
         public static Newtonsoft.Json.Linq.JToken POST(string URL, Dictionary<string, string> Headers = null,string sPostData="")
         {
+            if (CachedObjectExists(URL, Headers, sPostData)) { return GetCachedResponse(URL, Headers, sPostData); }
             WebRequest Request = WebRequest.Create(ConfigHandler.Config["API"]["URL"].ToString() + ":" + ConfigHandler.Config["API"]["Port"] +URL);
             Request.Method = "POST";
             byte[] PostData = Encoding.UTF8.GetBytes(sPostData);
@@ -35,8 +38,60 @@ namespace OwlCoinV2.Backend.Shared
             WebResponse Response = Request.GetResponse();
             string D = new StreamReader(Response.GetResponseStream()).ReadToEnd();
             Newtonsoft.Json.Linq.JToken JD = Newtonsoft.Json.Linq.JToken.Parse(D);
+            CacheResponse(URL, Headers, sPostData, JD);
             return JD;
         }
 
+        static List<CachedResponse> CachedResponses = new List<CachedResponse> { };
+        static void CacheResponse(string URL,Dictionary<string,string> Headers,string sPostData,Newtonsoft.Json.Linq.JToken Response)
+        {
+            if ((Headers != null || sPostData != null) && !CompatiblePostURLs.Contains(URL)) { return; }
+            foreach (CachedResponse Resp in CachedResponses) { if (ObjectMatches(URL, Headers, sPostData, Resp)) { CachedResponses.Remove(Resp); break; } }
+            CachedResponses.Add(new CachedResponse(URL, sPostData, Headers,Response));
+        }
+
+        static Newtonsoft.Json.Linq.JToken GetCachedResponse(string URL, Dictionary<string, string> Headers, string sPostData)
+        {
+            if (CachedObjectExists(URL, Headers, sPostData))
+            {
+                foreach (CachedResponse Resp in CachedResponses) { if (ObjectMatches(URL, Headers, sPostData, Resp)) { return Resp.Response; } }
+            }
+            return null;
+        }
+
+        static List<string> CompatiblePostURLs = new List<string> { "/user" };
+        static bool CachedObjectExists(string URL, Dictionary<string, string> Headers, string sPostData)
+        {
+            if ((Headers != null || sPostData != null) && !CompatiblePostURLs.Contains(URL)) { return false; }
+            foreach (CachedResponse Resp in CachedResponses) { if (ObjectMatches(URL,Headers,sPostData,Resp)&&(DateTime.Now-Resp.DateTime).TotalMilliseconds<2000) { return true; } }
+            return false;
+        }
+
+        static bool ObjectMatches(string URL,Dictionary<string,string> Headers,string sPostData,CachedResponse CachedResponse)
+        {
+            if (URL != CachedResponse.URL) return false;
+            if (sPostData != CachedResponse.sPostData) return false;
+            foreach (KeyValuePair<string,string> KeyPair in Headers)
+            {
+                if (!CachedResponse.Headers.Keys.Contains(KeyPair.Key)) return false;
+                if (CachedResponse.Headers[KeyPair.Key] != KeyPair.Value) return false;
+            }
+            return true;
+        }
+
+    }
+
+    class CachedResponse
+    {
+        public string URL, sPostData;
+        public Dictionary<string, string> Headers;
+        public Newtonsoft.Json.Linq.JToken Response;
+        public DateTime DateTime;
+
+        public CachedResponse(string url,string PostData,Dictionary<string,string> Header,Newtonsoft.Json.Linq.JToken response)
+        {
+            URL = url;sPostData = PostData;Headers = Header;Response = response;
+            DateTime = DateTime.Now;
+        }
     }
 }
