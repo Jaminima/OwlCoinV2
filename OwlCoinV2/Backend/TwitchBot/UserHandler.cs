547f9f26cf7a7a6663d3f5445933914cba286b94
Oscar Davies <oscar.william.davies@outlook.com>
2018-12-21 15:45:27 +0000

    On TwitchAPI failure try again a max of 10 times.

--- OwlCoinV2/Backend/TwitchBot/UserHandler.cs
+++ OwlCoinV2/Backend/TwitchBot/UserHandler.cs
@@ -13,24 +13,26 @@ namespace OwlCoinV2.Backend.TwitchBot
 {
     public static class UserHandler
     {
-        public static TwitchLib.Api.Models.v5.Users.Users UserFromUsername(string Username)
+        public static TwitchLib.Api.Models.v5.Users.Users UserFromUsername(string Username,int i=0)
         {
+            if (i == 10) { return null; }
             try
             {
                 Task<TwitchLib.Api.Models.v5.Users.Users> GetUName = new TwitchLib.Api.Sections.Users.V5Api(Bot.TwitchA).GetUserByNameAsync(Username);
                 return Task.Run(async () => await new TwitchLib.Api.Sections.Users.V5Api(Bot.TwitchA).GetUserByNameAsync(Username)).Result;
             }
-            catch (Exception E) { Console.WriteLine(E); return null; }
+            catch (Exception E) { Console.WriteLine(E); return UserFromUsername(Username,i++); }
         }
 
-        public static TwitchLib.Api.Models.v5.Users.User UserFromUserID(string UserID)
+        public static TwitchLib.Api.Models.v5.Users.User UserFromUserID(string UserID,int i=0)
         {
+            if (i == 10) { return null; }
             try
             {
                 Task<TwitchLib.Api.Models.v5.Users.User> GetUName = new TwitchLib.Api.Sections.Users.V5Api(Bot.TwitchA).GetUserByIDAsync(UserID);
                 return Task.Run(async () => await new TwitchLib.Api.Sections.Users.V5Api(Bot.TwitchA).GetUserByIDAsync(UserID)).Result;
             }
-            catch (Exception E) { Console.WriteLine(E); return null; }
+            catch (Exception E) { Console.WriteLine(E); return UserFromUserID(UserID,i++); }
         }
 
     }
